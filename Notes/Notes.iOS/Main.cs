8ced5691402a2da7f1c8c1d31682a96cc48cefec
Pivnev Vladislav <steneox@gmail.com>
2021-11-18 19:26:05 +0300

    Добавьте файлы проекта.

---
+++ Notes/Notes.iOS/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace Notes.iOS
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+#pragma warning disable CS0618 // "UIApplication.Main(string[]?, string?, string?)" является устаревшим: 'Use the overload with 'Type' instead of 'String' parameters for type safety.'
+            UIApplication.Main(args, null, "AppDelegate");
+#pragma warning restore CS0618 // "UIApplication.Main(string[]?, string?, string?)" является устаревшим: 'Use the overload with 'Type' instead of 'String' parameters for type safety.'
+        }
+    }
+}
